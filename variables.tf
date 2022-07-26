5728ecdc22594ffc9202409b425be52f8e9ec2fe
Nagaraju <nagaraju7225@gmail.com>
2022-07-26 15:10:20 +0200

    added local file

--- variables.tf
+++ variables.tf
@@ -8,9 +8,3 @@ variable "vpccidr" {
   type = string
   default = "192.168.0.0/16"
 }
-variable "subnets" {
-    type = list(string)
-    default = ["web1", "web2", "app1", "app2", "db1", "db2"]
-    description = "Name of the subnets"
-}
-
