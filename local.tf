75283e89b50bfa0877395845a17d75b6b4ba9470
Nagaraju <nagaraju7225@gmail.com>
2022-07-27 09:26:12 +0200

    added sg

--- local.tf
+++ local.tf
@@ -2,4 +2,9 @@ locals {
   subnets = ["web1", "web2", "app1", "app2", "db1", "db2"]
   igw_name = "ntiergw"
   anywhere = "0.0.0.0/0"
+  ssh = 22
+  http = 80
+  tcp = "tcp"
+  appport = 8080
+  dpport = 3306
 }
--- main.tf
+++ main.tf
@@ -84,3 +84,85 @@ resource "aws_route_table_association" "applassociation" {
     aws_route_table.privatert
   ]
 }
+#create a web security group 
+resource "aws_security_group" "websg" {
+  name = "websg"
+  description = "open 22 and 80 port for all"
+  vpc_id = aws_vpc.ntiervpc.id
+  
+  ingress {
+    cidr_blocks = [local.anywhere]
+    description = "open ssh port"
+    from_port = local.ssh
+    protocol = local.tcp
+    to_port = local.ssh
+  }
+
+  ingress {
+    cidr_blocks = [local.anywhere]
+    description = "open http port"
+    from_port = local.http
+    protocol = local.tcp
+    to_port = local.http
+  }
+
+  tags = {
+    "Name" = "websg"
+  }
+  
+  depends_on = [
+    aws_route_table.privatert,
+    aws_route_table.publicrt
+  ]
+}
+resource "aws_security_group" "appsg" {
+  name = "appsg"
+  description = "open port 8080 and 22 within vpc"
+  vpc_id = aws_vpc.ntiervpc.id
+
+  ingress {
+    cidr_blocks = [ var.vpccidr ]
+    description = "open ssh port"
+    from_port = local.ssh
+    protocol = local.tcp
+    to_port = local.ssh
+  }
+
+  ingress {
+    cidr_blocks = [ var.vpccidr ]
+    description = "open app port"
+    from_port = local.appport
+    protocol = local.tcp
+    to_port = local.appport
+  }
+
+  tags = {
+    "Name" = "appsg"
+  }
+  depends_on = [
+    aws_route_table.privatert,
+    aws_route_table.publicrt
+  ]
+}
+resource "aws_security_group" "dbsg" {
+  name = "dbsg"
+  description = "open port 3306 with in vpc"
+  vpc_id = aws_vpc.ntiervpc.id
+
+  ingress {
+    cidr_blocks = [ var.vpccidr ]
+    description = "open db port"
+    from_port = local.dpport
+    protocol = local.tcp
+    to_port = local.dpport
+  }
+
+  tags = {
+    "Name" = "dbsg"
+  }
+
+  depends_on = [
+    aws_route_table.privatert,
+    aws_route_table.publicrt
+  ]
+}
